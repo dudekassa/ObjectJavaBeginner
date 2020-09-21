f9a57c7baf1fbd807fc1573dba7a54415cfbbcb0
Przemysław Dudek <dudek.assa@gmail.com>
2020-09-21 06:25:21 +0000

    First

---
+++ Human.java
@@ -0,0 +1,16 @@
+public class Human {
+
+  private String name;
+
+  private String surname;
+
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+}
