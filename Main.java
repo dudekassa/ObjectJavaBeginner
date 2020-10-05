7cae5f45b3f3fdde330596c7c47b0b7c48eeb007
Przemysław Dudek <dudek.assa@gmail.com>
2020-10-05 06:35:02 +0000

    05 10 2020 zadanie 2

--- Main.java
+++ Main.java
@@ -1,19 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Main {
+
   public static void main(String[] args) {
     System.out.println("Hello world!");
 
     Human dziecko = new Child("Janek", "Nowak");
     Human dorosly = new Adult("Piotr", "Kowalski");
 
-    dziecko.sayHello();
+    //dziecko.sayHello();
+    //dorosly.sayHello();
 
-    dorosly.sayHello();
+    // int[] arrayInt = new int[3];
+    // for (int i = 0; i < arrayInt.length; i++) {
+    //   arrayInt[i] = i;
+    // }
+    // for (int i = 0; i < arrayInt.length; i++) {
+    //   System.out.println(arrayInt[i]);
+    // }
 
     List<Human> humanList = new ArrayList<>();
 
+    humanList.add(dorosly);
     humanList.add(dziecko);
 
-    
+    for (Human human : humanList) {
+      System.out.println();
+      human.introduceYourself();
+      human.sayHello();
+      System.out.println();
+    }
 
   }
 }
